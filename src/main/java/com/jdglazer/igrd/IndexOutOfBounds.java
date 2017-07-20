a359c1c4e64d6fe492b42a121d5b1fae5142d2ad
atrail2014@gmail.com <jglazer@localhost.localdomain>
2017-07-19 23:50:40 -0400

    initial commit

---
+++ src/main/java/com/jdglazer/igrd/IndexOutOfBounds.java
@@ -0,0 +1,7 @@
+package com.jdglazer.igrd;
+
+public class IndexOutOfBounds extends Exception {
+	public IndexOutOfBounds( String msg ) {
+		super( msg );
+	}
+}
---
+++ src/main/java/com/jdglazer/igrd/utils/InvalidFileTypeException.java
@@ -0,0 +1,9 @@
+package com.jdglazer.igrd.utils;
+public class InvalidFileTypeException extends Exception {
+	
+	InvalidFileTypeException() {}
+	
+	public InvalidFileTypeException(String msg) {
+		super(msg);
+	}
+}
